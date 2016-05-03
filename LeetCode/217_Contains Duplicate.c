d8d05a4f58253b8cffcc65975a287ac75a767c28
jakezjf <jianfeng95@outlook.com>
2016-05-03 10:16:55 +0800

    update
    java

--- LeetCode/217_Contains Duplicate.c
+++ LeetCode/217_Contains Duplicate.c
@@ -10,3 +10,16 @@ bool containsDuplicate(int* nums, int numsSize) {
     }
     return false;
 }
+
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
