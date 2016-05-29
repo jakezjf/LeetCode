2bb6032fe224d9ceec13a10c757ba676f6077d2b
Neve <jianfeng95@outlook.com>
2016-05-29 22:43:14 +0800

    Update 171_Excel Sheet Column Number.java

--- LeetCode/171_Excel Sheet Column Number.java
+++ LeetCode/171_Excel Sheet Column Number.java
@@ -1,1 +1,26 @@
 
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+public int titleToNumber(String s) {
+        if (s == null)
+            return 0;
+        int result = 0;
+        int j = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int c = (int)s.charAt(i) - 64; 
+            result += c * (int)Math.pow(26, j);
+            j++;
+        }
+        return result;
+    }
