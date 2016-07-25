27902f146c085040493fdf09e4e8c33d5f6a9424
Neve <jianfeng95@outlook.com>
2016-07-25 09:35:48 +0800

    Update 349_Intersection of Two Arrays.java

--- LeetCode/349_Intersection of Two Arrays.java
+++ LeetCode/349_Intersection of Two Arrays.java
@@ -1,3 +1,8 @@
+/**
+ * Given two arrays, write a function to compute their intersection.Example:
+ * Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+ * 
+**/
 public int[] intersection(int[] nums1, int[] nums2) {
 
 
