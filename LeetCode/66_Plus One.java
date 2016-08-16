36f48235327920ce26b0bf55a09637a45a72bc78
Neve <jianfeng95@outlook.com>
2016-08-16 18:35:30 +0800

    Create 66_Plus One.java

---
+++ LeetCode/66_Plus One.java
@@ -0,0 +1,25 @@
+/**
+*
+*Given a non-negative number represented as an array of digits, plus one to the number.
+*The digits are stored such that the most significant digit is at the head of the list.
+*
+*/
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i] ++;
+                break;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        if(digits[0]==0){
+            int[] arr = new int[digits.length+1];
+            arr[0] = 1;
+            return arr;
+        }
+        return digits;
+    }
+}
