4e294145c1e4491d4e015402b1976a80dfc409a6
Neve <jianfeng95@outlook.com>
2016-08-19 14:02:04 +0800

    Update 121_Best Time to Buy and Sell Stock.java

--- LeetCode/121_Best Time to Buy and Sell Stock.java
+++ LeetCode/121_Best Time to Buy and Sell Stock.java
@@ -14,3 +14,22 @@ Output: 0
 In this case, no transaction is done, i.e. max profit = 0.
 *
 */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0){
+            return 0;
+        }
+        int temp = prices[0];
+        int count = 0;
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i]<temp){
+                temp = prices[i];
+            }else{
+                if(prices[i] - temp >count){
+                    count = prices[i] - temp;
+                }
+            }
+        }
+        return count;
+    }
+}
