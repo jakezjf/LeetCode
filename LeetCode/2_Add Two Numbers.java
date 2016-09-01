583c8ef9ccbe06ba85d19b5f7a9714f6dfca1c50
Neve <jianfeng95@outlook.com>
2016-09-01 14:20:04 +0800

    Update 2_Add Two Numbers.java

--- LeetCode/2_Add Two Numbers.java
+++ LeetCode/2_Add Two Numbers.java
@@ -19,6 +19,17 @@ public class Solution {
         ListNode rhead = l1;
         ListNode list1 = l1;
         ListNode list2 = l2;
+        if(list1.next==null && list2.next==null){
+            list1.val = list1.val + list2.val;
+            if(list1.val>=10){
+                ListNode temp = new ListNode(0);
+                list1.val = list1.val - 10;
+                temp.next = null;
+                temp.val = 1;
+                list1.next = temp;
+            }
+            return rhead;
+        }
         while(list1.next!=null && list2.next!=null){
             if((list1.val + list2.val)>=10){
                 list1.next.val = list1.next.val + 1;
@@ -26,11 +37,65 @@ public class Solution {
             }else{
                 list1.val = list1.val + list2.val;
             }
-            System.out.println(list1.val);
             list1 = list1.next;
             list2 = list2.next;
         }
         list1.val = list1.val + list2.val;
+        if(list2.next!=null){
+            System.out.println(list2.val);
+            list2 = list2.next; 
+            list1.next = list2;
+            if(list1.val>=10){
+                list1.val = list1.val - 10;
+                int t = 1;
+                while(list2.next!=null){
+                    list2.val = list2.val + t;
+                    if(list2.val>=10){
+                        list2.val = list2.val -10;
+                        t = 1;
+                    }else{
+                        t = 0;
+                    }
+                    System.out.println(list2.val);
+                    list2 = list2.next; 
+                }
+                list2.val = list2.val + t;
+                if(list2.val >= 10){
+                    ListNode temp = new ListNode(0);
+                    list2.val = list2.val - 10;
+                    temp.next = null;
+                    temp.val = 1;
+                    list2.next = temp;
+                }
+            }
+            return rhead;
+        }
+        if(list1.next!=null){
+            if(list1.val>=10){
+                list1.val = list1.val - 10;
+                int t = 1;
+                list1 = list1.next;
+                while(list1.next!=null){
+                    list1.val = list1.val + t;
+                    if(list1.val>=10){
+                        list1.val = list1.val -10;
+                        t = 1;
+                    }else{
+                        t = 0;
+                    }
+                    list1 = list1.next; 
+                }
+                list1.val = list1.val + t;
+                if(list1.val>=10){
+                    ListNode temp = new ListNode(0);
+                    list1.val = list1.val - 10;
+                    temp.next = null;
+                    temp.val = 1;
+                    list1.next = temp;
+                }
+            }
+            return rhead;
+        }
         if(list1.val>=10){
             ListNode temp = new ListNode(0);
             list1.val = list1.val - 10;
