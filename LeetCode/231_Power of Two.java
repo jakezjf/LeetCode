f8736b826317fe0faf771c2095e98ec8cdad4698
Neve <jianfeng95@outlook.com>
2016-09-09 12:50:04 +0800

    Create 231_Power of Two.java

---
+++ LeetCode/231_Power of Two.java
@@ -0,0 +1,18 @@
+/**
+*Given an integer, write a function to determine if it is a power of two.
+*/
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        while (n % 2 == 0) {
+		    n = n / 2;
+	    }
+	    if(n==1)
+	        return true;
+	    return false;
+    }
+}
+
