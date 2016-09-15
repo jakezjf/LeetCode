1115b5cc4689318436502e95c464256dda1709fd
Neve <jianfeng95@outlook.com>
2016-09-15 17:38:38 +0800

    Update 153_Find Minimum in Rotated Sorted Array.java

--- LeetCode/153_Find Minimum in Rotated Sorted Array.java
+++ LeetCode/153_Find Minimum in Rotated Sorted Array.java
@@ -7,6 +7,19 @@
 *
 *You may assume no duplicate exists in the array.
 *
+* 1ms
 */
 
-
+public class Solution {
+    public int findMin(int[] nums) {
+      int i =1;
+      int temp = nums[0];
+      while(i<nums.length){
+          if(nums[i]<temp){
+              temp = nums[i];
+          }
+          i++;
+      }
+      return temp;
+    }
+}
