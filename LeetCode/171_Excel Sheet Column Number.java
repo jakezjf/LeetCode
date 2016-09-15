c8702da624ee4bb95a2986decf1e7c7bf4b47eeb
Neve <jianfeng95@outlook.com>
2016-09-15 18:19:21 +0800

    add a solution

--- LeetCode/171_Excel Sheet Column Number.java
+++ LeetCode/171_Excel Sheet Column Number.java
@@ -1,26 +1,39 @@
-
-Related to question Excel Sheet Column Title
-
-Given a column title as appear in an Excel sheet, return its corresponding column number.
-
-For example:
-
-    A -> 1
-    B -> 2
-    C -> 3
-    ...
-    Z -> 26
-    AA -> 27
-    AB -> 28 
-public int titleToNumber(String s) {
-        if (s == null)
-            return 0;
-        int result = 0;
-        int j = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int c = (int)s.charAt(i) - 64; 
-            result += c * (int)Math.pow(26, j);
-            j++;
+/**
+*Related to question Excel Sheet Column Title
+*
+*Given a column title as appear in an Excel sheet, return its corresponding column number.
+*
+*For example:
+*
+*   A -> 1
+*    B -> 2
+*    C -> 3
+*    ...
+*    Z -> 26
+*    AA -> 27
+*   AB -> 28 
+*/
+// public int titleToNumber(String s) {
+//         if (s == null)
+//             return 0;
+//         int result = 0;
+//         int j = 0;
+//         for (int i = s.length() - 1; i >= 0; i--) {
+//             int c = (int)s.charAt(i) - 64; 
+//             result += c * (int)Math.pow(26, j);
+//             j++;
+//         }
+//         return result;
+//     }
+    
+public class Solution {
+    public int titleToNumber(String s) {
+        int sum = 0;
+        for (int i = 0 ;i<s.length() ;i++ ){
+            sum = sum * 26 + ((int)s.charAt(i)-64);
         }
-        return result;
+        return sum;
     }
+}    
+
+//2ms
