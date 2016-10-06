f238a445806a2ff29524c9125a7e4ca985649281
Neve <jianfeng95@outlook.com>
2016-10-06 19:53:10 +0800

    增加第二种解决方法

--- LeetCode/155_Min Stack.java
+++ LeetCode/155_Min Stack.java
@@ -1,3 +1,21 @@
+/**
+*Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+*
+*push(x) -- Push element x onto stack.
+*pop() -- Removes the element on top of the stack.
+*top() -- Get the top element.
+*getMin() -- Retrieve the minimum element in the stack.
+*Example:
+*MinStack minStack = new MinStack();
+*minStack.push(-2);
+*minStack.push(0);
+*minStack.push(-3);
+*minStack.getMin();   --> Returns -3.
+*minStack.pop();
+*minStack.top();      --> Returns 0.
+*minStack.getMin();   --> Returns -2.
+*/
+
 public class MinStack {
     private Stack<Integer> stackData;
 
@@ -54,3 +72,49 @@ public class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+// public class MinStack {
+//     private Stack<Integer> stackData;
+
+//     private Stack<Integer> stackMin;
+
+//     /** initialize your data structure here. */
+//     public MinStack() {
+//         this.stackData = new Stack<Integer>();
+//         this.stackMin = new Stack<Integer>();
+//     }
+
+//     public void push(int x) {
+//        if(this.stackMin.isEmpty()){
+//             this.stackMin.push(x);
+//         }
+//         else if(x < this.getMin()){
+//             this.stackMin.push(x);
+//         }else{
+//             this.stackMin.push(this.getMin());
+//         }
+//         this.stackData.push(x);
+//     }
+
+//     public void pop() {
+//         if(this.stackData.isEmpty()){
+//             return;
+//         }
+//         this.stackMin.pop();
+//         this.stackData.pop();
+//     }
+
+//     public int top() {
+//         if(this.stackData.isEmpty()){
+//             return 0;
+//         }
+//         return this.stackData.peek();
+//     }
+
+//     public int getMin() {
+//         if(this.stackMin.isEmpty()){
+//             return 0;
+//         }
+//         return this.stackMin.peek();
+//     }
+// }
