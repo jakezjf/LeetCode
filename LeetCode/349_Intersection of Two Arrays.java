d86d795756babfeaefe32f4d53174a1191373429
Neve <jianfeng95@outlook.com>
2016-12-14 13:06:59 +0800

    Update 349_Intersection of Two Arrays.java

--- LeetCode/349_Intersection of Two Arrays.java
+++ LeetCode/349_Intersection of Two Arrays.java
@@ -40,3 +40,37 @@ private int[] list2array(List<Integer> comm) {
 
     return ret;
 }
+
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> m = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] == nums2[j]){
+                m.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] > nums2[j]){
+                j++;
+            } else if (nums1[i] < nums2[j]){
+                i++;
+            }
+        }
+        int[] arr = new int[m.size()];
+        int index = 0;
+        for(Integer t : m){
+            arr[index++] = t;
+        }
+        return arr;
+    }
+}
+
+
+
